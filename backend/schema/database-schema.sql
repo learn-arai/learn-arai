7ef062a75d33d2d64137e5760d35d8653ed9407e
Athicha L. (mac) <nongtonkaew@gmail.com>
2024-02-12 12:55:00 +0700

    Added ematil verficiation to database schema

--- backend/schema/database-schema.sql
+++ backend/schema/database-schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS auth_user (
     id TEXT PRIMARY KEY,
 
     email TEXT UNIQUE NOT NULL,
+    emal_verified BOOLEAN NOT NULL DEFAULT FALSE,
     hashed_password TEXT NOT NULL,
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -11,4 +12,12 @@ CREATE TABLE IF NOT EXISTS user_session (
     id TEXT PRIMARY KEY,
     expires_at TIMESTAMPTZ NOT NULL,
     user_id TEXT NOT NULL REFERENCES auth_user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS auth_email_verification (
+    id TEXT PRIMARY KEY DEFAULT gen_random_uuid(),
+    code CHAR(6) NOT NULL,
+    user_id TEXT UNIQUE NOT NULL REFERENCES auth_user(id) ON DELETE CASCADE,
+    email TEXT,
+    expires_at TIMESTAMPTZ
 );
