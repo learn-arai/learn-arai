efbcf81ab57ec96125d502ef729737dee9ebe37f
Athicha L. (mac) <nongtonkaew@gmail.com>
2024-03-03 12:58:15 +0700

    Added drawer & dialog components

--- learn-arai/components/classrooom/navbar/navbar.tsx
+++ learn-arai/components/classrooom/navbar/navbar.tsx
@@ -3,8 +3,10 @@ import { FaPlus } from 'react-icons/fa6';
 export default function Navbar() {
     return (
         <>
-            <nav className="flex items-center shadow p-6">
-                <FaPlus className="mx-auto mr-0" />
+            <nav className="flex items-center shadow p-3 px-6">
+                <button className="hover:bg-muted mx-auto mr-0 p-3 rounded-full">
+                    <FaPlus className="" />
+                </button>
             </nav>
         </>
     );
