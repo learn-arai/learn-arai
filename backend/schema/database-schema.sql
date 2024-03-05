f52ffcaf5e8b7f6af5ae599a7b35817093f41edb
Athicha L. (mac) <nongtonkaew@gmail.com>
2024-03-05 15:19:05 +0700

    Added ticker database schema

--- backend/schema/database-schema.sql
+++ backend/schema/database-schema.sql
@@ -1,8 +1,10 @@
 CREATE TYPE PACKAGE_TYPE AS ENUM ('free', 'premium');
+CREATE TYPE USER_TYPE AS ENUM ('user', 'supporter');
 
 CREATE TABLE IF NOT EXISTS auth_user (
     id      TEXT PRIMARY KEY,
     package PACKAGE_TYPE NOT NULL DEFAULT 'free',
+    type    USER_TYPE NOT NULL DEFAULT 'user',    
 
     email           TEXT UNIQUE NOT NULL,
     email_verified  BOOLEAN NOT NULL DEFAULT FALSE,
@@ -62,4 +64,26 @@ CREATE TABLE IF NOT EXISTS teach (
 
     added_by TEXT REFERENCES auth_user(id),
     added_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS ticket (
+    id           TEXT PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id      TEXT NOT NULL REFERENCES auth_user(id) ON DELETE CASCADE,
+    supporter_id TEXT NOT NULL REFERENCES auth_user(id) ON DELETE CASCADE,
+    is_close     BOOLEAN NOT NULL DEFAULT FALSE,
+
+    title       TEXT NOT NULL,
+    description TEXT NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS ticket_message (
+    id        TEXT PRIMARY KEY DEFAULT gen_random_uuid(),
+    ticket_id TEXT NOT NULL REFERENCES ticket(id) ON DELETE CASCADE,
+    user_id   TEXT NOT NULL REFERENCES auth_user(id) ON DELETE CASCADE,
+    
+    content   TEXT NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
