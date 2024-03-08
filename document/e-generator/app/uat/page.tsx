2ae9ace423d9a32e6b286789309fc6fe24b51445
tonkaew131 <nongtonkaew@gmail.com>
2024-03-09 00:03:46 +0700

    Added prettier

---
+++ document/e-generator/app/uat/page.tsx
@@ -0,0 +1,12 @@
+import { PDFViewer } from '@react-pdf/renderer';
+import UATDocument from './uat-document';
+
+export default function Page() {
+    return (
+        <>
+            <PDFViewer className="h-screen w-full">
+                <UATDocument />
+            </PDFViewer>
+        </>
+    );
+}
