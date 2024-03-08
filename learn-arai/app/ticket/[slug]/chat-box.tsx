b5bef96b19eee932a8d0909351173f30e7763a8e
Athicha L. (mac) <nongtonkaew@gmail.com>
2024-03-08 10:17:41 +0700

    Added ticket list box

---
+++ learn-arai/app/ticket/[slug]/chat-box.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export default function ChatBox({ slug }: { slug: string }) {
+    const chatRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.addEventListener('keypress', function (event) {
+                if (event.key === 'Enter') {
+                    event.preventDefault();
+
+                    sendMessage();
+                }
+            });
+        }
+    });
+
+    const sc = useRef<WebSocket>();
+    if (!sc.current) {
+        sc.current = new WebSocket(`ws://localhost:3000/ticket/${slug}/chat`);
+
+        sc.current.addEventListener('message', (event) => {
+            const { message, type, createdAt } = JSON.parse(event.data);
+
+            if (type == 'system') {
+                const p = document.createElement('div');
+                p.className =
+                    'text-center text-sm text-muted-foreground py-4 font-mono';
+                p.textContent = message;
+                chatRef.current?.appendChild(p);
+                return;
+            }
+
+            const div = document.createElement('div');
+            div.className =
+                'ds-chat ' + (type == 'you' ? 'ds-chat-end' : 'ds-chat-start');
+
+            const bubble = document.createElement('div');
+            bubble.className =
+                'ds-chat-bubble ' +
+                (type == 'other'
+                    ? 'bg-primary text-primary-foreground'
+                    : 'bg-muted text-muted-foreground');
+            bubble.textContent = message;
+            div.appendChild(bubble);
+
+            const footer = document.createElement('div');
+            footer.className = 'ds-chat-footer opacity-50 text-xs pt-1';
+            footer.textContent = new Date(createdAt).toLocaleTimeString(
+                'th-TH'
+            );
+            div.appendChild(footer);
+
+            chatRef.current?.appendChild(div);
+        });
+    }
+
+    const sendMessage = () => {
+        if (!sc.current) return;
+        sc.current.send(
+            JSON.stringify({
+                message: inputRef.current?.value,
+            })
+        );
+
+        inputRef.current!.value = '';
+    };
+
+    return (
+        <div className="h-full relative px-4">
+            <div className="" ref={chatRef}></div>
+
+            <Label className="flex gap-2 items-center bottom-0 absolute w-full left-1/2 -translate-x-1/2 px-4">
+                <Input ref={inputRef} />
+                <Button className="px-8" onClick={sendMessage}>
+                    Send
+                </Button>
+            </Label>
+        </div>
+    );
+}
--- learn-arai/app/ticket/[slug]/page.tsx
+++ learn-arai/app/ticket/[slug]/page.tsx
@@ -1,90 +1,54 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { FaAngleLeft } from 'react-icons/fa';
+
+import { Card, CardContent } from '@/components/ui/card';
+import Footer from '@/components/ui/footer/footer';
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from '@/components/ui/resizable';
+import { Separator } from '@/components/ui/separator';
+
+import ChatBox from './chat-box';
 
 export default function Page({ params }: { params: { slug: string } }) {
-    const chatRef = useRef<HTMLDivElement>(null);
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    useEffect(() => {
-        if (inputRef.current) {
-            inputRef.current.addEventListener('keypress', function (event) {
-                if (event.key === 'Enter') {
-                    event.preventDefault();
-
-                    sendMessage();
-                }
-            });
-        }
-    });
-
-    const sc = useRef<WebSocket>();
-    if (!sc.current) {
-        sc.current = new WebSocket(
-            `ws://localhost:3000/ticket/${params.slug}/chat`
-        );
-
-        sc.current.addEventListener('message', (event) => {
-            const { message, type, createdAt } = JSON.parse(event.data);
-
-            if (type == 'system') {
-                const p = document.createElement('div');
-                p.className =
-                    'text-center text-sm text-muted-foreground py-4 font-mono';
-                p.textContent = message;
-                chatRef.current?.appendChild(p);
-                return;
-            }
-
-            const div = document.createElement('div');
-            div.className =
-                'ds-chat ' + (type == 'you' ? 'ds-chat-end' : 'ds-chat-start');
-
-            const bubble = document.createElement('div');
-            bubble.className =
-                'ds-chat-bubble ' +
-                (type == 'other'
-                    ? 'bg-primary text-primary-foreground'
-                    : 'bg-muted text-muted-foreground');
-            bubble.textContent = message;
-            div.appendChild(bubble);
-
-            const footer = document.createElement('div');
-            footer.className = 'ds-chat-footer opacity-50 text-xs pt-1';
-            footer.textContent = new Date(createdAt).toLocaleTimeString(
-                'th-TH'
-            );
-            div.appendChild(footer);
-
-            chatRef.current?.appendChild(div);
-        });
-    }
-
-    const sendMessage = () => {
-        if (!sc.current) return;
-        sc.current.send(
-            JSON.stringify({
-                message: inputRef.current?.value,
-            })
-        );
-
-        inputRef.current!.value = '';
-    };
-
     return (
         <>
-            <div className="max-w-xl" ref={chatRef}></div>
+            <div className="h-screen w-screen max-w-full">
+                <ResizablePanelGroup direction="horizontal">
+                    <ResizablePanel defaultSize={25}>
+                        <Card className="rounded-none relative">
+                            <CardContent className="py-4 font-semibold text-center">
+                                <h2>My Tickets</h2>
+                            </CardContent>
 
-            <Label className="max-w-xl flex gap-2 items-center">
-                <Input ref={inputRef} />
-                <Button className="px-8" onClick={sendMessage}>
-                    Send
-                </Button>
-            </Label>
+                            <Link
+                                href="/ticket"
+                                className="absolute left-4 top-1/2 -translate-y-1/2"
+                            >
+                                <FaAngleLeft className="" />
+                            </Link>
+                        </Card>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel className="flex flex-col flex-1 pb-8">
+                        <Card className="rounded-none">
+                            <CardContent className="py-4 font-semibold text-center">
+                                <h2>{params.slug}</h2>
+                            </CardContent>
+                        </Card>
+
+                        <ChatBox slug={params.slug} />
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
+
+            <Separator />
+            <Footer />
         </>
     );
 }
