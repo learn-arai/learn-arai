1ca2b4f29e0e25439049492c63b745ecd3c177b6
tonkaew131 <nongtonkaew@gmail.com>
2024-03-09 10:25:26 +0700

    Added UAT Header & Its documents

--- document/e-generator/app/uat/uat-document.tsx
+++ document/e-generator/app/uat/uat-document.tsx
@@ -1,37 +1,68 @@
 'use client';
-import React, { useEffect } from 'react';
-import {
-    Page,
-    Text,
-    View,
-    Document,
-    StyleSheet,
-    Font,
-} from '@react-pdf/renderer';
+import React, { useEffect, useState } from 'react';
+import { Page, Text, View, Document, Font } from '@react-pdf/renderer';
 import { parse } from 'csv-parse/sync';
+import { styles } from './styles';
 
 Font.register({
     family: 'Noto Serif',
-    src: '/Noto_Serif_Thai/NotoSerifThai-VariableFont_wdth,wght.ttf',
+    fonts: [
+        {
+            src: '/Noto_Serif_Thai/static/NotoSerifThai-Regular.ttf',
+            fontWeight: 'normal',
+        },
+        {
+            src: '/Noto_Serif_Thai/static/NotoSerifThai-Bold.ttf',
+            fontWeight: 'bold',
+        },
+    ],
 });
 
-const styles = StyleSheet.create({
-    page: {
-        backgroundColor: '#ffffff',
-        padding: '40 20 50 20',
-        fontFamily: 'Noto Serif',
-        fontSize: 12,
-    },
-});
+interface UATHeader {
+    code: string; // รหัสทดสอบ
+    testDate: string; // วันที่ทดสอบ
+    system: string; // ระบบส่วนงาน
+    workName: string; // ชื่อการทำงาน
+    screenCode: string; // รหัสหน้าจอ - ชื่อหน้าจอ
+    description: string; // คำอธิบาย
+    condition: string; // เงื่อนไขก่อนการทำงาน
+    process: string; // ขั้นตอนการทำงาน
+    expected: string; // ผลที่คาดว่าจะได้รับ
+    result: string; // ผลการทดสอบ
+    note: string; // บันทึกเพิ่มเติม
+}
 
 export default function UATDocument() {
+    const [uatData, setUATData] = useState<UATHeader[] | null>(null);
+
     const getUATFile = async () => {
+        if (uatData !== null) return;
+
         const res = await fetch('/uat/uat-2024-mar-09-00-12.csv');
         const data = await res.text();
 
-        const records = parse(data, { delimiter: ',' });
-
+        const records: any[][] = parse(data, { delimiter: ',' });
         console.log(records);
+        records.shift();
+
+        const parsedRecords: UATHeader[] = [];
+        for (let i = 0; i < records.length; i++) {
+            parsedRecords.push({
+                code: records[i][0],
+                testDate: records[i][1],
+                system: records[i][2],
+                workName: records[i][3],
+                screenCode: records[i][4],
+                description: records[i][5],
+                condition: records[i][6],
+                process: records[i][7],
+                expected: records[i][8],
+                result: records[i][9],
+                note: records[i][10],
+            });
+        }
+
+        setUATData(parsedRecords);
     };
 
     useEffect(() => {
@@ -40,9 +71,70 @@ export default function UATDocument() {
 
     return (
         <Document>
-            <Page size="A4" style={styles.page}>
-                <Text>Hello World!</Text>
-            </Page>
+            {uatData?.map((data, idx) => (
+                <Page size="A4" style={styles.page} key={data.code}>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            รหัสทดสอบ{' '}
+                        </Text>
+                        <Text style={styles['tableItem-1/4']}>
+                            {data.code}{' '}
+                        </Text>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            วันที่ทดสอบ{' '}
+                        </Text>
+                        <Text style={styles['tableItem-1/4']}></Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            ระบบ/ส่วนงาน{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.system}{' '}
+                        </Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            ชื่อการทำงาน{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.workName}{' '}
+                        </Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            รหัสหน้าจอ-ชื่อหน้าจอ{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.screenCode}{' '}
+                        </Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            คำอธิบาย{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.description}{' '}
+                        </Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            เงื่อนไขก่อนการทำงาน{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.condition}{' '}
+                        </Text>
+                    </View>
+                    <View style={styles.tableRow}>
+                        <Text style={styles['tableItem-1/4-bold']}>
+                            ขั้นตอนการทำงาน{' '}
+                        </Text>
+                        <Text style={styles['tableItem-3/4']}>
+                            {data.process}{' '}
+                        </Text>
+                    </View>
+                </Page>
+            ))}
         </Document>
     );
 }
