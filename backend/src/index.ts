c15a9c3ad74e77ed2af06b1969928e1bb0c1a1db
tonkaew131 <40863067+tonkaew131@users.noreply.github.com>
2024-03-11 17:01:38 +0000

    Moved classroom group api router into separate file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { Elysia } from 'elysia';
 
 import { authRoute } from '@route/auth';
 import { classroomRoute } from '@route/classroom';
+import { classroomGroupRoute } from '@route/classroom-group';
 import { fileRoute } from '@route/file';
 
 const app = new Elysia()
@@ -36,6 +37,7 @@ const app = new Elysia()
     })
     .use(authRoute)
     .use(classroomRoute)
+    .use(classroomGroupRoute)
     .use(fileRoute)
     .get('/', () => 'Hello Elysia world')
     .use(cors())
---
+++ backend/src/routes/classroom-group.ts
@@ -0,0 +1,73 @@
+import { Elysia, t } from 'elysia';
+
+import { sql } from '@/lib/db';
+import { generateSlug } from '@/lib/utils';
+import { middleware } from '@/src/middleware';
+
+export const classroomGroupRoute = new Elysia({ prefix: '/classroom' }).group(
+    '/:slug/group',
+    (app) =>
+        app.use(middleware).post(
+            '/create',
+            async ({ params, user, session, set, body }) => {
+                if (!user || !session) {
+                    set.status = 401;
+                    return {
+                        status: 'error',
+                        message:
+                            'Unauthenticated, Please sign in and try again',
+                    };
+                }
+
+                const { slug } = params;
+
+                const classroom = await sql`
+                SELECT
+                    teach.classroom_id
+                FROM teach
+                INNER JOIN classroom
+                    ON classroom.id = teach.classroom_id
+                WHERE
+                    classroom.slug = ${slug} AND
+                    teach.user_id = ${user.id}
+                `;
+
+                if (classroom.length === 0) {
+                    set.status = 404;
+
+                    return {
+                        status: 'error',
+                        message:
+                            "Classroom not found or you're not the teacher of this classroom.",
+                    };
+                }
+
+                const { title } = body;
+                const { classroom_id: classroomId } = classroom[0];
+                const groupSlug = generateSlug();
+
+                await sql`
+                    INSERT INTO classroom_group
+                        (title, classroom_id, created_by, slug)
+                    VALUES
+                        (${title}, ${classroomId}, ${user.id}, ${groupSlug})
+                `;
+
+                return {
+                    status: 'success',
+                    message: 'Group has been created.',
+                    data: {
+                        slug: groupSlug,
+                    },
+                };
+            },
+            {
+                params: t.Object({
+                    slug: t.String(),
+                }),
+                body: t.Object({
+                    title: t.String(),
+                }),
+            },
+        ),
+);
