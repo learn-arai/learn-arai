c6a21d79764fb0338b1fee5cd51d7d3c844660ba
tonkaew131 <40863067+tonkaew131@users.noreply.github.com>
2024-03-12 09:10:47 +0000

    Fixed command item not able to click

--- learn-arai/app/classroom/[slug]/page.tsx
+++ learn-arai/app/classroom/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 
 import SlugContext from '@/components/context/SlugContext';
 import CreateInvite from '@/components/module/classrooom/create-invited-code/create-invited-code';
-import ChipInput from '@/components/ui/chip-input';
+import { ComboboxDemo } from '@/components/ui/combo-box';
 
 export default function Page() {
     const slug = useContext(SlugContext);
@@ -12,7 +12,9 @@ export default function Page() {
     return (
         <>
             <h1>Slug is {slug}</h1>
-            <CreateInvite />
+            {/* <CreateInvite /> */}
+
+            <ComboboxDemo />
         </>
     );
 }
