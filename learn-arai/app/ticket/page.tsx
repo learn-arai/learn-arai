aa6e751ad4ae11593200f9b3bf40a9fd019b3f2b
tonkaew131 <40863067+tonkaew131@users.noreply.github.com>
2024-03-12 15:17:32 +0700

    Support ticket system (#31)
    * Added ticker database schema
    
    * Added websocket ticket route api
    
    * Moved ticket chat to dynamic route
    
    * Added landing page for ticker support
    
    * Added support ticket ui
    
    * Added footer to support page
    
    * Update ticket link in footer
    
    * Fixed landing mobile responsive
    
    * Use daisy ui as chat bubble
    
    * Added chat history api
    
    * Added ticket list box
    
    * Added linked to ticket chat
    
    * Fixed support should see all tickets
    
    * Show supporter chat box they're supporter
    
    * Added createTicket hooks
    
    * Moved create ticket form into its component
    
    * Show ticket date
    
    * Added chat list beside chat box
    
    * Fixed overflow ticket title
    
    * Sorted chat history by date
    
    * Added ability to close ticket
    
    * Refresh ticket when create ticket
    
    * Scroll new msg into view
    
    * Fixed close ticket not update chatbox
    
    * Create env for websocket url

---
+++ learn-arai/app/ticket/page.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+import { useContext, useEffect } from 'react';
+import { useFormState } from 'react-dom';
+import { IoIosChatboxes } from 'react-icons/io';
+import { LuTicket } from 'react-icons/lu';
+import { MdOutlineSupportAgent } from 'react-icons/md';
+
+import { formatDate } from '@/lib/utils';
+
+import { TicketContext } from '@/components/context/TicketContext';
+import { useTicket } from '@/components/hooks/useTicket';
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import Footer from '@/components/ui/footer/footer';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+
+export default function Page() {
+    const ticket = useContext(TicketContext);
+
+    return (
+        <>
+            <div className="px-32 py-12">
+                <div className="prose pb-0">
+                    <h2 className="flex items-center gap-2">
+                        <LuTicket />
+                        My Tickets
+                    </h2>
+                </div>
+
+                <div className="flex gap-6 pb-2 my-6 max-w-full overflow-x-scroll">
+                    {(!ticket.history || ticket.history.length == 0) && (
+                        <p className="text-muted-foreground font-semibold text-center py-24 text-sm w-full">
+                            You have no tickets yet.
+                        </p>
+                    )}
+                    {ticket.history &&
+                        ticket.history.map((v) => (
+                            <div key={v.slug}>
+                                <Card className="w-72 h-full flex flex-col justify-between">
+                                    <CardHeader>
+                                        <CardTitle className="text-lg">
+                                            {v.title}
+                                        </CardTitle>
+                                        <CardDescription className="line-clamp-5">
+                                            {v.description}
+                                        </CardDescription>
+                                    </CardHeader>
+                                    <CardFooter className="flex flex-col items-start">
+                                        <Link
+                                            href={`/ticket/${v.slug}`}
+                                            className="w-full"
+                                        >
+                                            <Button className="w-full flex items-center gap-2">
+                                                Chat
+                                                <IoIosChatboxes />
+                                            </Button>
+                                        </Link>
+
+                                        <p className="text-sm text-muted-foreground">
+                                            {formatDate(v.createdAt)}
+                                        </p>
+                                    </CardFooter>
+                                </Card>
+                            </div>
+                        ))}
+                </div>
+
+                <div className="prose pb-6">
+                    <h2 className="flex items-center gap-2">
+                        <MdOutlineSupportAgent />
+                        Submit a ticket
+                    </h2>
+                </div>
+
+                <CreateTicketForm />
+            </div>
+
+            <Separator />
+            <Footer />
+        </>
+    );
+}
+
+function CreateTicketForm() {
+    const { createTicket } = useTicket();
+    const ticket = useContext(TicketContext);
+
+    const [state, formAction] = useFormState(createTicket, {
+        status: 'idle',
+    });
+
+    useEffect(() => {
+        if (state.status === 'success') {
+            ticket.updateTicket();
+            redirect(`/ticket/${state.data.slug}`);
+        }
+    }, [state, ticket]);
+
+    return (
+        <form className="max-w-lg grid items-start gap-4" action={formAction}>
+            <div className="grid gap-2">
+                <Label htmlFor="title">Title</Label>
+                <Input type="text" id="title" name="title" />
+            </div>
+
+            <div className="grid gap-2">
+                <Label htmlFor="description">Description</Label>
+                <Textarea
+                    id="description"
+                    name="description"
+                    className="min-h-[200px]"
+                />
+            </div>
+
+            <Button className="w-fit px-12" type="submit">
+                Submit
+            </Button>
+        </form>
+    );
+}
--- learn-arai/lib/utils.ts
+++ learn-arai/lib/utils.ts
@@ -1,6 +1,88 @@
 import { type ClassValue, clsx } from 'clsx';
+import { customAlphabet } from 'nanoid';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string | Date) {
+    return new Date(date).toLocaleString('en-TH', {
+        hour: 'numeric',
+        minute: 'numeric',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+}
+
+export function timeAgo(date: string | Date, simple: Boolean = false): string {
+    let age = Date.now() - new Date(date).getTime();
+
+    if (age / 1000 < 5) {
+        return 'just now';
+    }
+
+    const out = [];
+
+    const YEAR = 1000 * 60 * 60 * 24 * 365;
+    const year = Math.floor(age / YEAR);
+    age -= year * YEAR;
+
+    if (year > 0) {
+        out.push(`${year} year${year > 1 ? 's' : ''}`);
+        if (simple) return out.join(' ');
+    }
+
+    const month = Math.floor(age / (YEAR / 12));
+    age -= month * (YEAR / 12);
+
+    if (month > 0) {
+        out.push(`${month} month${month > 1 ? 's' : ''}`);
+        if (simple) return out.join(' ');
+    }
+
+    const day = Math.floor(age / (YEAR / 365));
+    age -= day * (YEAR / 365);
+
+    if (day > 0) {
+        out.push(`${day} day${day > 1 ? 's' : ''}`);
+        if (simple) return out.join(' ');
+    }
+
+    if (month == 0) {
+        const hour = Math.floor(age / (1000 * 60 * 60));
+        age -= hour * (1000 * 60 * 60);
+
+        if (hour > 0) {
+            out.push(`${hour} hour${hour > 1 ? 's' : ''}`);
+            if (simple) return out.join(' ');
+        }
+
+        const minute = Math.floor(age / (1000 * 60));
+        age -= minute * (1000 * 60);
+
+        if (minute > 0) {
+            out.push(`${minute} minute${minute > 1 ? 's' : ''}`);
+            if (simple) return out.join(' ');
+        }
+
+        const second = Math.floor(age / 1000);
+        age -= second * 1000;
+
+        if (second > 0) {
+            out.push(`${second} second${second > 1 ? 's' : ''}`);
+            if (simple) return out.join(' ');
+        }
+    }
+
+    return out.join(' ');
+}
+
+export function generateNanoId(size = 10) {
+    const nanoid = customAlphabet(
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        size
+    );
+    return nanoid();
 }
