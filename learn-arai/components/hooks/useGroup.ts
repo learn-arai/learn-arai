0b4296bc11dccd845805a171c776224804546ab5
Salamander <mokmaard646@gmail.com>
2024-03-14 18:31:10 +0700

    connect createGroup function with API endpoint

---
+++ learn-arai/components/hooks/useGroup.ts
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import SlugContext from "../context/SlugContext"
+
+
+export function useGroup(){
+  const slug = useContext(SlugContext);
+
+  const createNewGroup = async (formData : FormData) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/c/${slug}/g/create`, {
+      method: 'POST',
+      credentials : 'include',
+      body : formData
+    })
+
+    const data = await response.json();
+    return data;
+  }
+
+  return { createNewGroup };
+}
