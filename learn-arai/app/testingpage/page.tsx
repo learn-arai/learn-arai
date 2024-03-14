420e4f57b5bf139088982132d63ae634dd319d1c
Gapraomoosap <peelasin2544@gmail.com>
2024-03-15 01:59:46 +0700

    make member list components

---
+++ learn-arai/app/testingpage/page.tsx
@@ -0,0 +1,14 @@
+import List from "@/components/ui/member-list"
+
+export default function home() {
+    return (
+        <div className="flex">
+            <div className="border-4 w-4/5 h-[950px]">
+
+            </div>
+            <div className="w-1/5">
+                <List></List>
+            </div>
+        </div>
+    )
+}
--- learn-arai/components/ui/member-list.tsx
+++ learn-arai/components/ui/member-list.tsx
@@ -1,10 +1,30 @@
 
-export default function list() {
-
+export default function List() {
+    const teacher_list = [{
+        name: 'peerasin',
+        surname: 'srisri'
+    },
+    {
+        name: 'mok',
+        surname: 'maraard'
+    }]
+    const student_list = []
     return (
-        <>
-
-        </>
+        <div className="flex flex-col gap-2 w-full">
+            <h2>Teacher</h2>
+            {teacher_list.map(function (data) {
+                return (
+                    <div className="flex flex-row gap-2 border-4">
+                        <div>
+                            {data.name}
+                        </div>
+                        <div>
+                            {data.surname}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 
 }
