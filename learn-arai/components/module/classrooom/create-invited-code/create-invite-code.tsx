f8828038e50641f2f2dca00bc670be4715550185
Salamander <mokmaard646@gmail.com>
2024-03-16 14:32:41 +0700

    create selected state

--- learn-arai/components/module/classrooom/create-invited-code/create-invite-code.tsx
+++ learn-arai/components/module/classrooom/create-invited-code/create-invite-code.tsx
@@ -8,6 +8,7 @@ import { useFormState } from 'react-dom';
 import { FaPlus } from 'react-icons/fa6';
 import { FaXmark } from 'react-icons/fa6';
 import { GrGroup } from 'react-icons/gr';
+import { MdCancel } from 'react-icons/md';
 
 import { cn } from '@/lib/utils';
 
@@ -15,7 +16,6 @@ import SlugContext from '@/components/context/SlugContext';
 import { useClassroom } from '@/components/hooks/useClassroom';
 import { useMediaQuery } from '@/components/hooks/useMediaQuery';
 import { Button } from '@/components/ui/button';
-import CodeLine from '@/components/ui/code-line';
 import { ComboBox } from '@/components/ui/combo-box';
 import {
     Dialog,
@@ -37,7 +37,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 import './input-chip.css';
-import { MdCancel } from 'react-icons/md';
 
 function CreateInviteButton(props: React.ComponentProps<'button'>) {
     return (
@@ -93,31 +92,59 @@ export default function CreateInvite() {
 }
 
 export type Group = {
-    label: string;
-    value: string;
-    isSelected: boolean;
+    slug: string;
+    title: string;
+    isSelected : boolean;
 };
+
+type SelectedGroup = {
+    [key : string] : string;
+    // key will be slug and value will be title;
+    // all elements that in here are the groups that be selected by user.
+}
 
 function CreateInviteForm({ className }: React.ComponentProps<'form'>) {
     const slug = React.useContext(SlugContext);
-    const [selectedGroup, setSelectedGroup] = useState<Group[]>([]);
-    const { createInviteCode } = useClassroom();
-
+    const [selectedGroup, setSelectedGroup] = useState<SelectedGroup[]>([]);
+    const [allGroups, setAllGroups] = useState<Group[]>([]);
+    const { createInviteCode, getGroupList } = useClassroom();
     const [state, formAction] = useFormState(createInviteCode, {
         status: 'idle',
+        slug: slug,
     });
 
-    const deleteChip = (label: string) => {
-        setSelectedGroup((prev) => {
-            for (let i = 0; i < selectedGroup.length; i++) {
-                if (prev[i].label == label) {
-                    prev[i].isSelected = false;
-                    break;
-                }
-            }
+    React.useEffect(() => {
+        getAllGroups();
+    });
 
-            return [...prev];
-        });
+    // const deleteChip = (label: string) => {
+    //     const groupList = getGroupList(slug);
+
+    //     setSelectedGroup((prev) => {
+    //         for (let i = 0; i < selectedGroup.length; i++) {
+    //             if (prev[i].label == label) {
+    //                 prev[i].isSelected = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         return [...prev];
+    //     });
+    // };
+
+    const getAllGroups = async () => {
+        const groupList = await getGroupList(slug);
+
+        if (groupList.status === 'success') {
+            setAllGroups( groupList.data.map((group) => {
+                return {
+                    slug: group.slug,
+                    title: group.title,
+                    isSelected: false,
+                };
+            }
+            ));
+        }
     };
 
     return (
@@ -125,7 +152,7 @@ function CreateInviteForm({ className }: React.ComponentProps<'form'>) {
             className={cn('grid items-start gap-4', className)}
             action={formAction}
         >
-            <input
+            {/* <input
                 type="hidden"
                 name="group_slug"
                 value={(() => {
@@ -137,25 +164,25 @@ function CreateInviteForm({ className }: React.ComponentProps<'form'>) {
                     }
                     return JSON.stringify(selectedDatas);
                 })()}
-            />
+            /> */}
 
             {/* under construct */}
 
-            <ComboBox data={selectedGroup} setData={setSelectedGroup} />
-            
-            <ul className="table">
-                        {selectedGroup.map((row) => {
-                            if (row.isSelected) {
-                                return (
-                                    <Chip
-                                        key={row.value}
-                                        label={row.label}
-                                        deleteChip={deleteChip}
-                                    />
-                                );
-                            }
-                        })}
-            </ul>
+            <ComboBox data={allGroups} setData={setAllGroups} />
+
+            {/* <ul className="table">
+                {selectedGroup.map((row) => {
+                    if (row.isSelected) {
+                        return (
+                            <Chip
+                                key={row.value}
+                                label={row.label}
+                                deleteChip={deleteChip}
+                            />
+                        );
+                    }
+                })}
+            </ul> */}
 
             {/* under construct */}
 
@@ -201,7 +228,6 @@ function Chip({
         </li>
     );
 }
-
 
 function FormInput({
     name,
