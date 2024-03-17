4516ab24f2107a8388d1eb746a058df541489619
tonkaew131 <nongtonkaew@gmail.com>
2024-03-17 14:11:55 +0700

    Skip test register ;P

--- backend/test/auth.test.ts
+++ backend/test/auth.test.ts
@@ -3,37 +3,33 @@ import { describe, expect, test } from 'bun:test';
 const apiURL = process.env.API_URL;
 
 describe('Auth System', () => {
-    describe('Register', () => {
-        test.skip('Normal', async () => {
-            const body = new FormData();
-            body.append('email', 'johndoe@gmail.com');
-            body.append('password', 'aA112233');
-            body.append('password-confirmation', 'aA112233');
-            body.append('name', 'John');
-            body.append('surname', 'Doe');
-            body.append('phone', '012-345-6789');
+    test.skip('Register', async () => {
+        const body = new FormData();
+        body.append('email', 'johndoe@gmail.com');
+        body.append('password', 'aA112233');
+        body.append('password-confirmation', 'aA112233');
+        body.append('name', 'John');
+        body.append('surname', 'Doe');
+        body.append('phone', '012-345-6789');
 
-            const response = await fetch(`${apiURL}/auth/sign-up`, {
-                method: 'POST',
-                body,
-            });
-            const json = await response.json();
+        const response = await fetch(`${apiURL}/auth/sign-up`, {
+            method: 'POST',
+            body,
+        });
+        const json = await response.json();
 
-            expect(json).toMatchObject({
-                status: 'success',
-                message: 'Please check your email for code verification',
-            });
+        expect(json).toMatchObject({
+            status: 'success',
+            message: 'Please check your email for code verification',
         });
     });
 
-    describe('Login', () => {
-        test('Normal', async () => {
-            const { json } = await signIn('johndoe@gmail.com', 'aA112233');
+    test('Login', async () => {
+        const { json } = await signIn('johndoe@gmail.com', 'aA112233');
 
-            expect(json).toMatchObject({
-                status: 'success',
-                message: 'login success',
-            });
+        expect(json).toMatchObject({
+            status: 'success',
+            message: 'login success',
         });
     });
 });
