ea1c00981c387aff5c7a66d2bd0dc7536e864bd6
Salamander <mokmaard646@gmail.com>
2024-03-26 19:22:16 +0700

    in frontend, change backend URL accroding to change of name API endpoint

--- learn-arai/components/hooks/useClassroom.ts
+++ learn-arai/components/hooks/useClassroom.ts
@@ -236,7 +236,7 @@ export const useClassroom = () => {
 
     const getChatGroupList = async (slug: string) => {
         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}/c/${slug}/g/list/user-groups`,
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/c/${slug}/g/list/?is_student=true`,
             {
                 method: 'GET',
                 credentials: 'include',
