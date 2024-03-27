2ca9e0bd64d8d1b96a78e981b8eafb69560a7a54
Salamander <mokmaard646@gmail.com>
2024-03-28 01:05:47 +0700

    remove fixed height and unused import.

--- learn-arai/components/module/classroom/chat-room/chat-room.tsx
+++ learn-arai/components/module/classroom/chat-room/chat-room.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { Chat } from './chat';
 import { GroupList } from './group-list';
@@ -8,21 +8,21 @@ export function ChatRoom() {
     const [currentGroupSlug, setCurrentGroupSlug] = useState('');
 
     return (
-        <div className="flex">
-            <div className="w-2/12 bg-slate-50 overflow-y-auto h-[70vh]">
+        <>
+            <div className="w-2/12 bg-slate-50">
                 <GroupList
                     setGroupSlug={setCurrentGroupSlug}
                     groupSlug={currentGroupSlug}
                 />
             </div>
 
-            <div className="w-full relative bg-slate-100 h-[70vh]">
+            <div className="w-full bg-slate-100">
                 <Chat currentGroupSlug={currentGroupSlug} />
             </div>
 
-            <div className="w-3/12 bg-slate-50 p-2 overflow-y-auto h-[70vh]">
+            <div className="w-3/12 bg-slate-50 p-2">
                 <UserList currentGroupSlug={currentGroupSlug} />
             </div>
-        </div>
+        </>
     );
 }
