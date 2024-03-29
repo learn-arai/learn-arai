8623abd89361d822e2a0c45b9b8ab1bc68e5a6c2
tonkaew131 <nongtonkaew@gmail.com>
2024-03-29 17:41:33 +0700

    Added grader editor submit button & its result

--- learn-arai/app/classroom/[slug]/(grader)/grader/[grader-slug]/page.tsx
+++ learn-arai/app/classroom/[slug]/(grader)/grader/[grader-slug]/page.tsx
@@ -3,6 +3,14 @@
 import { useClassroomGrader } from '@/components/hooks/useClassroomGrader';
 import CodeEditor from '@/components/module/grader/code-editor';
 import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTriggerMinimal,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup,
@@ -21,7 +29,7 @@ export default function Page({
     const { data } = useGetDetail(graderSlug);
 
     return (
-        <div className="h-full">
+        <div className="h-full max-h-full">
             <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel className="">
                     {data && data.status === 'success' && (
@@ -29,8 +37,31 @@ export default function Page({
                     )}
                 </ResizablePanel>
                 <ResizableHandle withHandle />
-                <ResizablePanel className="">
-                    <CodeEditor />
+                <ResizablePanel className="flex flex-col max-h-full relative">
+                    <CodeEditor className="h-full" />
+
+                    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full p-2">
+                        <div className="flex items-center justify-end gap-2 mb-2">
+                            <Button
+                                variant="outline"
+                                size="sm"
+                                className="h-7 text-sm"
+                            >
+                                Run
+                            </Button>
+                            <Button
+                                variant="success"
+                                size="sm"
+                                className="h-7 text-sm"
+                            >
+                                Submit
+                            </Button>
+                        </div>
+                        <Card className="p-2">
+                            <span>Testcase</span>
+                            <span>Test Result</span>
+                        </Card>
+                    </div>
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
