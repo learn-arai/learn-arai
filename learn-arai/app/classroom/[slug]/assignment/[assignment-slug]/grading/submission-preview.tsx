f9488d2505634097c91acaaea2c200513e8d22d8
Gapraomoosap <peelasin2544@gmail.com>
2024-04-02 20:12:32 +0700

    add pdf components

--- learn-arai/app/classroom/[slug]/assignment/[assignment-slug]/grading/submission-preview.tsx
+++ learn-arai/app/classroom/[slug]/assignment/[assignment-slug]/grading/submission-preview.tsx
@@ -1,26 +1,36 @@
 'use client';
 
-import { useEffect } from "react";
+import PDFViewer from "@/components/ui/pdf-viewer";
+import { useEffect, useState } from "react";
 import { useClassroomAssignment } from "@/components/hooks/useClassroomAssignment";
-import { get } from "http";
+
+interface SubmissionFile {
+    file_id: string;
+    // Add other properties if there are more
+}
+
 export default function SubmissionPreview(props: {
-    userId: string ,
+    userId: string,
     classroomSlug: string,
     assignmentSlug: string,
 }) {
-    const { classroomSlug,userId,assignmentSlug } = props;
+    const { classroomSlug, userId, assignmentSlug } = props;
     const { getSubmissionFile } = useClassroomAssignment(classroomSlug);
-    
+    const [fileId, setFileId] = useState<SubmissionFile[]>([]); // Empty array of SubmissionFile objects
+
     useEffect(() => {
-        getSubmissionFile(userId,assignmentSlug);
+        const fetchData = async () => {
+            const file = await getSubmissionFile(userId, assignmentSlug);
+            setFileId(file.data);
+        };
+        fetchData();
     }, [userId]);
 
+    const FileId = fileId.length > 0 ? fileId[0].file_id :'';
+    console.log(FileId);
     return <>
-        <p>
-            {userId}
-        </p>
-        <p>
-            display user's file here
-        </p>
+        <PDFViewer
+            url={`${process.env.NEXT_PUBLIC_BACKEND_URL}/file/${FileId}.pdf`}
+        />
     </>
 }
