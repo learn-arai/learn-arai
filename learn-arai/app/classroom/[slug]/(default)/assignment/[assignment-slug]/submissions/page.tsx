32a41c640b4e990270d26cb71c6a898f47c09236
Gapraomoosap <peelasin2544@gmail.com>
2024-04-04 03:28:42 +0700

    create submission list page

--- learn-arai/app/classroom/[slug]/(default)/assignment/[assignment-slug]/submissions/page.tsx
+++ learn-arai/app/classroom/[slug]/(default)/assignment/[assignment-slug]/submissions/page.tsx
@@ -1,8 +1,6 @@
 'use client';
-
 import { useClassroomAssignment } from "@/components/hooks/useClassroomAssignment";
 import { useEffect, useState } from "react";
-
 
 export default function Page({
     params,
@@ -14,23 +12,92 @@ export default function Page({
         last_name: string;
         id: string;
         score: string;
+
     };
+
+    type ListType = {
+        num_assigned: number;
+        num_turned_in: number;
+        title: string;
+        assignment_slug: string;
+        max_score:string;
+    };
+
     const { slug, 'assignment-slug': assignmentSlug } = params;
     const { getUserSubmission } = useClassroomAssignment(slug);
-    const [data, setData] = useState<DataType[]>([]);
+    const { getAssignmentList } = useClassroomAssignment(slug);
+    const [list, setList] = useState<DataType[]>([]);
+    const [assignmentList, setAssignmentList] = useState<ListType[]>([]);
+
     useEffect(() => {
         const fetchData = async () => {
             const resName = await getUserSubmission(assignmentSlug);
-            setData(resName.data);
+            setList(resName.data);
+
         }
+
+        const fetchAssignment = async () => {
+            const resAssi = await getAssignmentList();
+            if (resAssi.status === "success") {
+                // Filter assignments by assignmentSlug
+                const formattedassignmentList: ListType[] = resAssi.data.map(assi => ({
+                    num_assigned: assi.num_assigned,
+                    num_turned_in: assi.num_turned_in,
+                    title: assi.title,
+                    assignment_slug: assi.slug,
+                    max_score : assi.max_score,
+                }));
+                const result: ListType[] = formattedassignmentList.filter((item) => item.assignment_slug === assignmentSlug);
+                setAssignmentList(result);
+            } else {
+                console.error("Error fetching assignment list");
+            }
+        }
+
+        fetchAssignment();
         fetchData();
     }, [assignmentSlug])
-    console.log('list = ',data);
+
+    // Access the title of the first item in assignmentList or use a default value if assignmentList is empty
+    const assignmentTitle = assignmentList.length > 0 ? assignmentList[0].title : "No Title";
+    const assignmentTurnedIn = assignmentList.length > 0 ? assignmentList[0].num_turned_in : "-";
+    const assignmentAssign = assignmentList.length > 0 ? assignmentList[0].num_assigned : "-";
+    const assignmentMaxscore = assignmentList.length > 0 ? assignmentList[0].max_score : "-";
 
 
     return (
         <>
-
+            <div className="flex flex-col mt-4">
+                <h1 className="font-normal">{assignmentTitle}</h1>
+                <div className="flex">
+                    <div className="turn-in flex flex-col border-r p-4">
+                        <p className="text-center text-2xl font-bold">{assignmentTurnedIn}</p>
+                        <p>Turned in</p>
+                    </div>
+                    <div className="assi flex flex-col p-4">
+                        <p className="text-center text-2xl font-bold">{assignmentAssign}</p>
+                        <p>Assigned</p>
+                    </div>
+                </div>
+                <div className="flex flex-col mt-4">
+                    <div className="flex justify-between mb-4">
+                        <p className="text-xl opacity-70">Name</p>
+                        <p className="text-xl opacity-70">score({assignmentMaxscore})</p>
+                    </div>
+                    {list.map((item, index) => (
+                        <div key={item.id} className="flex justify-between border-b pb-2">
+                            <div className="flex">
+                                <p className="text-2xl pr-4">{item.first_name}</p>
+                                <p className="text-2xl pr-4">{item.last_name}</p>
+                            </div>
+                            <div>
+                                <p className="text-2xl text-green-500">{item.score === null ? '-' : item.score}</p>
+                                
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
