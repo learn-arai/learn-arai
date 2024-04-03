cdd98a2dafcadfc1ab56227f9088cecc05d70b01
tonkaew131 <nongtonkaew@gmail.com>
2024-04-03 21:51:39 +0700

    Added add test-case test

--- backend/test/grader.ts
+++ backend/test/grader.ts
@@ -11,21 +11,21 @@ let graderData = {
 
 export default function runTest() {
     describe('Grader System', () => {
-        test('Create Grader Problem', async () => {
+        test('Create', async () => {
             const { cookie } = await signIn(
                 userTeacher1.email,
                 userTeacher1.password,
             );
 
             const instructionFile = await Bun.file(
-                'test/assets/week1_1.pdf',
+                'test/assets/average_shortest/avg_shortest.pdf',
             ).arrayBuffer();
 
             const body = new FormData();
             body.append('name', 'Lab1_1');
             body.append(
                 'instruction_file',
-                new File([instructionFile], 'week1_1.pdf', {
+                new File([instructionFile], 'avg_shortest.pdf', {
                     type: 'application/pdf',
                 }),
             );
@@ -51,6 +51,42 @@ export default function runTest() {
             expect(json.data).toContainKeys(['slug']);
 
             graderData.slug = json.data.slug;
+        });
+
+        test('Add test-case', async () => {
+            const { cookie } = await signIn(
+                userTeacher1.email,
+                userTeacher1.password,
+            );
+
+            const inputFile = await Bun.file(
+                'test/assets/average_shortest/1.in',
+            ).arrayBuffer();
+            const outputFile = await Bun.file(
+                'test/assets/average_shortest/1.sol',
+            ).arrayBuffer();
+
+            const body = new FormData();
+            body.append('score', '10');
+            body.append('input', new File([inputFile], '1.in'));
+            body.append('output', new File([outputFile], '1.sol'));
+
+            const response = await fetch(
+                `${apiURL}/c/${classroomData.classroomSlug}/gd/${graderData.slug}/add-test-case`,
+                {
+                    method: 'POST',
+                    headers: {
+                        cookie: cookie,
+                    },
+                    body,
+                },
+            );
+            const json = await response.json();
+
+            expect(json).toMatchObject({
+                status: 'success',
+                message: 'Test case added successfully',
+            });
         });
     });
 }
