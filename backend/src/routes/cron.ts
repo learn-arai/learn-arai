f9a6f9a35b970ded4c0c7696c8fbcdde05d4c1cc
Gapraomoosap <peelasin2544@gmail.com>
2024-04-04 11:12:14 +0700

    create cron to delete classroom

--- backend/src/routes/cron.ts
+++ backend/src/routes/cron.ts
@@ -1,16 +1,22 @@
 import { Elysia } from 'elysia'
 import { cron } from '@elysiajs/cron'
 
+import { sql } from '@/lib/db';
+
 new Elysia()
-    .state("A", "B")
     .use(
         cron({
-            name: 'heartbeat',
-            pattern: '*/5 * * * * *',
-            run() {
-                console.log('Heartbeat')
+            name: 'delete classroom',
+            pattern: '0 0-23/1 * * *',
+            async run() {
+                try {
+                    //TODO: Add notification
+                    const result = await sql`DELETE FROM classroom WHERE will_delete_in < NOW() `
+                    console.log('Deleted rows:', result)
+                  } catch (error) {
+                    console.error('Error deleting rows:', error)
+                  }
             }
         })
     )
-    .get('/', () => 'hello world!')
     .listen(3000)
