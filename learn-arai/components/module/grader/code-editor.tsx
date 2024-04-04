dd05d7c4d8500a962520047060d7046597eddce0
Salamander <mokmaard646@gmail.com>
2024-04-03 22:22:19 -0600

    Implement function to get value from monaco-editor.

--- learn-arai/components/module/grader/code-editor.tsx
+++ learn-arai/components/module/grader/code-editor.tsx
@@ -1,11 +1,50 @@
 'use client';
 
+import { useContext, useEffect, useRef } from 'react';
+
 import Editor from '@monaco-editor/react';
 
 import { cn } from '@/lib/utils';
+import SlugContext from '@/components/context/SlugContext';
 
-export default function CodeEditor(props: { className?: string }) {
+export default function CodeEditor(props: {
+    isSubmit: boolean;
+    setIsSubmit: (value: boolean) => void;
+    className?: string;
+    graderSlug: string;
+}) {
+    const classroomSlug = useContext(SlugContext);
     const { className } = props;
+    const editorRef = useRef<any>(null);
+    const defaultValue = `#include <iostream>
+using namespace std;
+    
+int main() {
+    return 0;
+}`;
+
+    const handleEditorDidMount = (editor: any, monaco: any) => {
+        editorRef.current = editor;
+    };
+
+    const submitSourceCode = async (sourceCode: string) => {
+        const formData = new FormData();
+        formData.append('source_code', sourceCode);
+
+        //TODO: still not woked, I don't know why
+        await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/c/${classroomSlug}/gd/${props.graderSlug}/submit`, {
+            credentials: 'include',
+            method: 'POST',
+            body: formData,
+        })
+    };
+
+    useEffect(() => {
+        props.setIsSubmit(false);
+        if (editorRef.current) {
+            submitSourceCode(editorRef.current.getValue());
+        }
+    }, [props.isSubmit]);
 
     return (
         <>
@@ -13,14 +52,11 @@ export default function CodeEditor(props: { className?: string }) {
                 className={cn('w-full', className)}
                 defaultLanguage="cpp"
                 theme="vs-dark"
-                defaultValue={`#include <iostream>
-
-using namespace std;
-
-int main() {
-    return 0;
-}`}
+                defaultValue={defaultValue}
+                onMount={handleEditorDidMount}
             />
+
+            
         </>
     );
 }
