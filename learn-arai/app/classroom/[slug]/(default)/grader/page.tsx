fe1ff0dda3b4f1940fa731b5cbf447d49000e608
tonkaew131 <nongtonkaew@gmail.com>
2024-04-20 20:25:39 +0700

    Only show create grader when you are teacher

--- learn-arai/app/classroom/[slug]/(default)/grader/page.tsx
+++ learn-arai/app/classroom/[slug]/(default)/grader/page.tsx
@@ -7,6 +7,7 @@ import { FaFileImport } from 'react-icons/fa6';
 
 import { PlusIcon } from 'lucide-react';
 
+import { ClassroomContext } from '@/components/context/ClassroomContext';
 import SlugContext from '@/components/context/SlugContext';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
@@ -14,29 +15,32 @@ import { Card, CardContent } from '@/components/ui/card';
 import ProblemList from './problem-list';
 
 export default function Page() {
+    const classroom = useContext(ClassroomContext);
     const slug = useContext(SlugContext);
 
     return (
         <>
             <div className="max-w-5xl mx-auto py-4 min-h-[60vh]">
-                <div className="flex items-center gap-2">
-                    <Link href={`/classroom/${slug}/grader/create`}>
-                        <Button className="flex gap-1 items-center">
-                            Create
-                            <PlusIcon className="w-4 h-4" />
-                        </Button>
-                    </Link>
+                {classroom?.type === 'teacher' && (
+                    <div className="flex items-center gap-2">
+                        <Link href={`/classroom/${slug}/grader/create`}>
+                            <Button className="flex gap-1 items-center">
+                                Create
+                                <PlusIcon className="w-4 h-4" />
+                            </Button>
+                        </Link>
 
-                    <Link href={`/classroom/${slug}/grader/import`}>
-                        <Button
-                            className="flex gap-1 items-center"
-                            variant="secondary"
-                        >
-                            Import zip
-                            <FaFileImport className="w-4 h-4" />
-                        </Button>
-                    </Link>
-                </div>
+                        <Link href={`/classroom/${slug}/grader/import`}>
+                            <Button
+                                className="flex gap-1 items-center"
+                                variant="secondary"
+                            >
+                                Import zip
+                                <FaFileImport className="w-4 h-4" />
+                            </Button>
+                        </Link>
+                    </div>
+                )}
 
                 <ProblemList />
 
