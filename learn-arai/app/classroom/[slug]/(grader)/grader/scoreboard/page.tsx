79fa8e375d647d06339e0373b1c9691f1d87432c
tonkaew131 <nongtonkaew@gmail.com>
2024-05-05 22:21:55 +0700

    Added scoreboard table

--- learn-arai/app/classroom/[slug]/(grader)/grader/scoreboard/page.tsx
+++ learn-arai/app/classroom/[slug]/(grader)/grader/scoreboard/page.tsx
@@ -1,11 +1,54 @@
+import { ListOrdered } from 'lucide-react';
+
 import { Card } from '@/components/ui/card';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
 export default function Page() {
     return (
         <>
             <div className="relative h-full bg-muted p-4">
-                <Card className="z-20 h-full bg-background p-4">
-                    hello world!
+                <Card className="h-full">
+                    <Table>
+                        <TableHeader>
+                            <TableRow className="*:p-2">
+                                <TableHead className="w-0 border-r">
+                                    <ListOrdered className="h-4 w-4" />
+                                </TableHead>
+                                <TableHead className="border-r">Name</TableHead>
+                                <TableHead>Score</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody className="border-b odd:bg-red-500">
+                            <TableRow className="*:p-2 odd:bg-green-500/5">
+                                <TableCell className="w-0 last:border-r-0 border-r text-center">
+                                    1.
+                                </TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                            </TableRow>
+                            <TableRow className="*:p-2 odd:bg-green-500/5">
+                                <TableCell className="w-0 last:border-r-0 border-r text-center">
+                                    2.
+                                </TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                            </TableRow>
+                            <TableRow className="*:p-2 odd:bg-green-500/5">
+                                <TableCell className="w-0 last:border-r-0 border-r text-center">
+                                    3.
+                                </TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                                <TableCell className="last:border-r-0 border-r">x</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
                 </Card>
             </div>
         </>
